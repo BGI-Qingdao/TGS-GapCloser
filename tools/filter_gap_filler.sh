11e0d582aade0be6b4c2d39c341368036e82c568
Guo Lidong <guolidong@genomics.cn>
2019-05-31 16:39:11 +0800

    update filter gap

--- tools/filter_gap_filler.sh
+++ tools/filter_gap_filler.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $# != 2 ]] ; then 
+    echo "usage : $0 gap_id input.txt "
+    exit 1 ;
+fi
+
 target=$1
 echo "Target gap id $target"
 echo "BEGIN{" >tmp.awk
@@ -12,3 +17,6 @@ echo "      printf(\">seq_id_%d\n%s\n\",id,\$12);" >>tmp.awk
 echo "      id+=1;" >>tmp.awk
 echo "  }">>tmp.awk
 echo "}" >>tmp.awk
+
+
+awk -f tmp.awk <$2 >ont.tmp.$target.fa
