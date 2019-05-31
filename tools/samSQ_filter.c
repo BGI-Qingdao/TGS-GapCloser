98f6c950ea62c5d26c5a95cad95391fd2230fa46
Guo Lidong <guolidong@genomics.cn>
2019-05-31 16:17:58 +0800

    add samSQ_filter.c

---
+++ tools/samSQ_filter.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+int main()
+{
+    std::string line ;
+    std::set<std::string> sqs ;
+
+    while( ! std::getline( std::cin , line ).eof() )
+    {
+        if( line.size() > 3 && line[0] == '@' && line[1] == 'S' && line[2] == 'Q' )
+        {
+            if( sqs.find(line) == sqs.end () )
+            {
+                sqs.insert(line) ;
+            }
+            else ;
+        }
+        else
+            std::cout<<line<<'\n';
+    }
+    return 0;
+}
