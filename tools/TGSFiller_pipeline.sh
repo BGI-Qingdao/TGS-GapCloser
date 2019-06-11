ca8a4ee240e9a51244f21a2c302e04c4b4c1f290
Lidong <guolidong@genomics.cn>
2019-06-11 08:39:36 +0800

    Update TGSFiller_pipeline.sh

--- tools/TGSFiller_pipeline.sh
+++ tools/TGSFiller_pipeline.sh
@@ -23,39 +23,39 @@ PILON_MEM="300G"
 #   generate sub-ont-fragments for all gaps .
 ###########################################################
 
-#$MINIMAP2  -x ava-ont -t 30 \
-#    $ONT_FA $CONTIG 1>$OUT_PREFIX.sub.paf 2>$OUT_PREFIX.minimap2.01.log || exit 1
-#
-#$BIN_DIR/ONTGapCandidate --ont_reads_a $ONT_FA --contig2ont_paf $OUT_PREFIX.sub.paf \
-#    <$SCAFF_INFO >$OUT_PREFIX.gap_info_seqs 2>$OUT_PREFIX.cand.log || exit 1
+$MINIMAP2  -x ava-ont -t 30 \
+    $ONT_FA $CONTIG 1>$OUT_PREFIX.sub.paf 2>$OUT_PREFIX.minimap2.01.log || exit 1
 
-#awk 'BEGIN{id=1;}{if($12 != "N"){gap_id=0+$1;printf(">seq_id_%d_%d\n%s\n",gap_id,id,$12);id+=1;}}' \
-#    <$OUT_PREFIX.gap_info_seqs >$OUT_PREFIX.ont.fasta || exit 1
+$BIN_DIR/ONTGapCandidate --ont_reads_a $ONT_FA --contig2ont_paf $OUT_PREFIX.sub.paf \
+    <$SCAFF_INFO >$OUT_PREFIX.gap_info_seqs 2>$OUT_PREFIX.cand.log || exit 1
+
+awk 'BEGIN{id=1;}{if($12 != "N"){gap_id=0+$1;printf(">seq_id_%d_%d\n%s\n",gap_id,id,$12);id+=1;}}' \
+    <$OUT_PREFIX.gap_info_seqs >$OUT_PREFIX.ont.fasta || exit 1
 
 ###########################################################
 # Step 2 :
 #   pilon the sub-ont-fragments .
 ###########################################################
 
-#$MINIMAP2 -t 30 -d $OUT_PREFIX.mmi $OUT_PREFIX.ont.fasta 1>$OUT_PREFIX.minimap2.02.log 2>&1 || exit 1
-#$MINIMAP2 -t 30 -k14 -w5 -n2 -m20 -s 40 --sr --frag yes  --split-prefix=rel3_prefix \
-#    -a $OUT_PREFIX.mmi  $READ12  1>$OUT_PREFIX.sam 2>$OUT_PREFIX.minimap2.03.log || exit 1
-#
-#$BIN_DIR/samSQ_filter < $OUT_PREFIX.sam  >$OUT_PREFIX.fiter.sam || exit 1
-#
-#$SAMTOOL view -bo $OUT_PREFIX.bam  $OUT_PREFIX.fiter.sam -@ 30 \
-#    >$OUT_PREFIX.samtool_01.log 2>&1 || exit 1
+$MINIMAP2 -t 30 -d $OUT_PREFIX.mmi $OUT_PREFIX.ont.fasta 1>$OUT_PREFIX.minimap2.02.log 2>&1 || exit 1
+$MINIMAP2 -t 30 -k14 -w5 -n2 -m20 -s 40 --sr --frag yes  --split-prefix=rel3_prefix \
+    -a $OUT_PREFIX.mmi  $READ12  1>$OUT_PREFIX.sam 2>$OUT_PREFIX.minimap2.03.log || exit 1
 
-#$SAMTOOL sort $OUT_PREFIX.bam -o $OUT_PREFIX.sort.bam -@ 30 \
-#    >$OUT_PREFIX.samtool_02.log 2>&1 || exit 1
-#
-#$SAMTOOL index $OUT_PREFIX.sort.bam -@ 30 \
-#    >$OUT_PREFIX.samtool_03.log 2>&1 || exit 1
+$BIN_DIR/samSQ_filter < $OUT_PREFIX.sam  >$OUT_PREFIX.fiter.sam || exit 1
 
-#$JAVA -Xmx$PILON_MEM -jar  $PILON --fix all \
-#    --genome $OUT_PREFIX.ont.fasta --bam $OUT_PREFIX.sort.bam \
-#    --output $OUT_PREFIX.ont.pilon --outdir ./ \
-#    --diploid  --threads 30 >$OUT_PREFIX.pilon.log 2>$OUT_PREFIX.pilon.err || exit 1
+$SAMTOOL view -bo $OUT_PREFIX.bam  $OUT_PREFIX.fiter.sam -@ 30 \
+    >$OUT_PREFIX.samtool_01.log 2>&1 || exit 1
+
+$SAMTOOL sort $OUT_PREFIX.bam -o $OUT_PREFIX.sort.bam -@ 30 \
+    >$OUT_PREFIX.samtool_02.log 2>&1 || exit 1
+
+$SAMTOOL index $OUT_PREFIX.sort.bam -@ 30 \
+    >$OUT_PREFIX.samtool_03.log 2>&1 || exit 1
+
+$JAVA -Xmx$PILON_MEM -jar  $PILON --fix all \
+    --genome $OUT_PREFIX.ont.fasta --bam $OUT_PREFIX.sort.bam \
+    --output $OUT_PREFIX.ont.pilon --outdir ./ \
+    --diploid  --threads 30 >$OUT_PREFIX.pilon.log 2>$OUT_PREFIX.pilon.err || exit 1
 
 ###########################################################
 # Step 3 :
@@ -63,8 +63,8 @@ PILON_MEM="300G"
 ###########################################################
 
 
-#$MINIMAP2  -x ava-ont -t 30 $OUT_PREFIX.ont.pilon.fasta $CONTIG \
-#    1>$OUT_PREFIX.fill.paf 2>$OUT_PREFIX.minimap2.04.log || exit 1
+$MINIMAP2  -x ava-ont -t 30 $OUT_PREFIX.ont.pilon.fasta $CONTIG \
+    1>$OUT_PREFIX.fill.paf 2>$OUT_PREFIX.minimap2.04.log || exit 1
 
 $BIN_DIR/ONTGapFiller --ont_reads_a $OUT_PREFIX.ont.pilon.fasta \
     --contig2ont_paf $OUT_PREFIX.fill.paf --work_mode 4 <$SCAFF_INFO \
