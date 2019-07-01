190e7d7c9ff6928de5036b8a8c1934f85e5ac0e5
Guo Lidong <guolidong@genomics.cn>
2019-07-01 16:59:24 +0800

    save pipeline

--- pipeline/pipeline.sh
+++ pipeline/pipeline.sh
@@ -1,63 +1,56 @@
 #!/bin/bash
 
 ###########################################################
-# Step 1 :
+# Step 0 :
 #   edit configuration .
 ###########################################################
 
-# make sure output prefix != input prefix
-OUT_PREFIX="test_03"
+#######################################
+# basic input files.
+#######################################
+# the input scaffold sequence file
+INPUT_SCAFF_FA="xxx"
+# the Third generation long reads ( raw data here )
+ONT_FA="/home/xumengyang/ONT/chr19_eva/Extract_Chr19_rel3.fa"
+
+#######################################
+# basic project settings.
+#######################################
+
+# prefix of output files.
+OUT_PREFIX="gapfill_test"
+
+#######################################
+# basic 3rd part tools settings.
+#######################################
+
 # this is a directory
 BIN_DIR="/dellfsqd1/ST_OCEAN/ST_OCEAN/USRS/guolidong/software/source_dir/TGSGapFiller/bin"
-
-# this is a file 
-CONTIG="fake25K_2KN.contig"
-# this is a file 
-SCAFF_INFO="fake25K_2KN.scaff_infos"
-# this is a file or ""
-ONT_FA="/home/xumengyang/ONT/chr19_eva/Extract_Chr19_rel3.fa"
-# this is a file or ""
-ONT_FQ=""
-# this is a file 
+# this is a executive file 
 MINIMAP2="/home/softwares/minimap2-master/minimap2"
 
 ###########################################################
-# Step 1.5 :
-#   check 
+# Step 1 :
+#   preprocess input file
 ###########################################################
 
-if [[ "$OUT_PREFIX.contig" == $CONTIG  || "$OUT_PREFIX.scaff_infos" == $SCAFF_INFO ]] ; then 
-    echo "ERROR OUTPUT PREFIX == INPUT PREFIX . exit ..."
-    exit 1;
-fi
+TMP_INPUT_SCAFTIG=$OUT_PREFIX".contig"
+TMP_INPUT_SCAFF_INFO=$OUT_PREFIX".input.scaff_infos"
+
+$BIN_DIR/SplitScaffSeq <$INPUT_SCAFF_FA >$TMP_INPUT_SCAFTIG 2>$TMP_INPUT_SCAFF_INFO
 
 ###########################################################
 # Step 2 :
-#   use ont reads to fill gaps
+#   use ont-sub-fragments to fill gaps
 ###########################################################
 
-if [[ $ONT_FA != "" ]] ; then 
-    $MINIMAP2  -x ava-ont -t 30 $ONT_FA $CONTIG  --sam-hit-only \
-        1>$OUT_PREFIX.fill.paf 2>$OUT_PREFIX.minimap2.04.log || exit 1
 
-    $BIN_DIR/ONTGapFiller --ont_reads_a $ONT_FA \
-        --contig2ont_paf $OUT_PREFIX.fill.paf --contig $CONTIG <$SCAFF_INFO \
-        >$OUT_PREFIX.scaff_infos 2>$OUT_PREFIX.fill.log || exit 1
-else
-    $MINIMAP2  -x ava-ont -t 30 $ONT_FQ $CONTIG  --sam-hit-only \
-        1>$OUT_PREFIX.fill.paf 2>$OUT_PREFIX.minimap2.04.log || exit 1
+$MINIMAP2  -x ava-ont -t 30 $ONT_FA $TMP_INPUT_SCAFTIG --sam-hit-only \
+    1>$OUT_PREFIX.fill.paf 2>$OUT_PREFIX.minimap2.04.log || exit 1
 
-    $BIN_DIR/ONTGapFiller --ont_reads_q $ONT_FQ \
-        --contig2ont_paf $OUT_PREFIX.fill.paf --contig $CONTIG <$SCAFF_INFO \
-        >$OUT_PREFIX.scaff_infos 2>$OUT_PREFIX.fill.log || exit 1
-fi
-
-if [[ ! -f $OUT_PREFIX.contig ]] ; then
-    ln -s $CONTIG $OUT_PREFIX.contig || exit 1
-else
-    echo "Warning : $OUT_PREFIX.contig already exist !!! use it anyway ."
-fi
+$BIN_DIR/ONTGapFiller --ont_reads_a $ONT_FA \
+    --contig2ont_paf $OUT_PREFIX.fill.paf --contig $TMP_INPUT_SCAFTIG <$TMP_INPUT_SCAFF_INFO\
+    >$OUT_PREFIX.scaff_infos 2>$OUT_PREFIX.fill.log || exit 1
 
 $BIN_DIR/ScaffInfo2Seq --prefix  $OUT_PREFIX --min_n 1 --min_c 0 \
     >$OUT_PREFIX.i2s.log 2>&1  || exit 1
-
--- pipeline/pipeline_polish.sh
+++ pipeline/pipeline_polish.sh
@@ -1,47 +1,77 @@
 #!/bin/bash
 
 ###########################################################
-# Step 1 :
+# Step 0 :
 #   edit configuration .
 ###########################################################
 
-OUT_PREFIX="test_03"
-BIN_DIR="/dellfsqd1/ST_OCEAN/ST_OCEAN/USRS/guolidong/software/source_dir/TGSGapFiller/bin"
-
-CONTIG="fake25K_2KN.contig"
-SCAFF_INFO="fake25K_2KN.scaff_infos"
-
-READ12="/home/guolidong/chr19/chr19_reads12.clean.fastq"
+#######################################
+# basic input files.
+#######################################
+# the input scaffold sequence file
+INPUT_SCAFF_FA="xxx"
+# the Third generation long reads ( raw data here )
 ONT_FA="/home/xumengyang/ONT/chr19_eva/Extract_Chr19_rel3.fa"
-MINIMAP2="/home/softwares/minimap2-master/minimap2"
-SAMTOOL="/home/softwares/samtools-1.9/bin/samtools"
-JAVA="/home/softwares/jdk-12.0.1/jdk-12.0.1/bin/java"
-PILON="/dellfsqd1/ST_OCEAN/ST_OCEAN/USRS/xumengyang/software/pilon-1.23.jar"
-COV=28
-PILON_MEM="300G"
+# NGS reads to polish above long reads. please concatenate read1 and read2 into 1 file. 
+READ12="/home/guolidong/chr19/chr19_reads12.clean.fastq"
 
+#######################################
+# basic project settings.
+#######################################
+
+# prefix of output files.
+OUT_PREFIX="gapfill_test"
+# java memory alloced for pilon running time.
+PILON_MEM="300G"
+# pilon chunk num.
+CHUNK_NUM=1
+
+#######################################
+# basic 3rd part tools settings.
+#######################################
+
+# this is a directory
+BIN_DIR="/dellfsqd1/ST_OCEAN/ST_OCEAN/USRS/guolidong/software/source_dir/TGSGapFiller/bin"
+# this is a executive file 
+MINIMAP2="/home/softwares/minimap2-master/minimap2"
+# this is a executive file 
+SAMTOOL="/home/softwares/samtools-1.9/bin/samtools"
+# this is a executive file 
+JAVA="/home/softwares/jdk-12.0.1/jdk-12.0.1/bin/java"
+# this is a jar file 
+PILON="/dellfsqd1/ST_OCEAN/ST_OCEAN/USRS/xumengyang/software/pilon-1.23.jar"
+
+###########################################################
+# Step 1 :
+#   preprocess input file
+###########################################################
+
+TMP_INPUT_SCAFTIG=$OUT_PREFIX".contig"
+TMP_INPUT_SCAFF_INFO=$OUT_PREFIX".input.scaff_infos"
+
+$BIN_DIR/SplitScaffSeq <$INPUT_SCAFF_FA >$TMP_INPUT_SCAFTIG 2>$TMP_INPUT_SCAFF_INFO
 ###########################################################
 # Step 2 :
 #   generate sub-ont-fragments for all gaps .
 ###########################################################
 
 $MINIMAP2  -x ava-ont -t 30 --sam-hit-only \
-    $ONT_FA $CONTIG 1>$OUT_PREFIX.sub.paf 2>$OUT_PREFIX.minimap2.01.log || exit 1
+    $ONT_FA $TMP_INPUT_SCAFTIG 1>$OUT_PREFIX.sub.paf 2>$OUT_PREFIX.minimap2.01.log || exit 1
 
 $BIN_DIR/ONTGapCandidate --ont_reads_a $ONT_FA --contig2ont_paf $OUT_PREFIX.sub.paf \
     <$SCAFF_INFO >$OUT_PREFIX.gap_info_seqs 2>$OUT_PREFIX.cand.log || exit 1
 
-for ((i=1; i<=COV; i++))
+for ((i=0; i<COV; i++))
 do
-    awk 'BEGIN{id=1;ii=0;prev=0}{if($12 != "N"){ gap_id=0+$1;ii=ii+1 ; if(prev!=gap_id){prev=gap_id;ii=1;}if(ii%(cov+1)==the_i){printf(">seq_id_%d_%d\n%s\n",gap_id,id,$12);}id+=1;}}' cov=$COV the_i=$i \
+    awk 'BEGIN{id=1;ii=0;prev=0}{if($12 != "N"){ gap_id=0+$1;ii=ii+1 ; if(prev!=gap_id){prev=gap_id;ii=1;}if(ii%(cov)==the_i){printf(">seq_id_%d_%d\n%s\n",gap_id,id,$12);}id+=1;}}' cov=$COV the_i=$i \
         <$OUT_PREFIX.gap_info_seqs >$OUT_PREFIX.ont.$i.fasta || exit 1
 done
 
 ###########################################################
-# Step 2 :
+# Step 3 :
 #   pilon the sub-ont-fragments .
 ###########################################################
-for ((i=1; i<=COV; i++))
+for ((i=0; i<COV; i++))
 do
     $MINIMAP2 -t 30 -d $OUT_PREFIX.mmi $OUT_PREFIX.ont.$i.fasta 1>$OUT_PREFIX.minimap2.02.log 2>&1 || exit 1
     $MINIMAP2 -t 30 -k14 -w5 -n2 -m20 -s 40 --sr --frag yes  --split-prefix=rel3_prefix --sam-hit-only \
@@ -63,30 +93,23 @@ do
         --output $OUT_PREFIX.ont.$i.pilon --outdir ./ \
         --diploid  --threads 30 >$OUT_PREFIX.pilon.log 2>$OUT_PREFIX.pilon.err || exit 1
 done
-for ((i=1; i<=COV; i++))
+for ((i=0; i<COV; i++))
 do
     cat $OUT_PREFIX.ont.$i.pilon.fasta >> $OUT_PREFIX.ont.pilon.fasta
 done
 
 ###########################################################
-# Step 3 :
+# Step 4 :
 #   use ont-sub-fragments to fill gaps
 ###########################################################
 
 
-$MINIMAP2  -x ava-ont -t 30 $OUT_PREFIX.ont.pilon.fasta $CONTIG  --sam-hit-only \
+$MINIMAP2  -x ava-ont -t 30 $OUT_PREFIX.ont.pilon.fasta $TMP_INPUT_SCAFTIG --sam-hit-only \
     1>$OUT_PREFIX.fill.paf 2>$OUT_PREFIX.minimap2.04.log || exit 1
 
 $BIN_DIR/ONTGapFiller --ont_reads_a $OUT_PREFIX.ont.pilon.fasta \
-    --contig2ont_paf $OUT_PREFIX.fill.paf --contig $CONTIG <$SCAFF_INFO \
+    --contig2ont_paf $OUT_PREFIX.fill.paf --contig $TMP_INPUT_SCAFTIG <$TMP_INPUT_SCAFF_INFO\
     >$OUT_PREFIX.scaff_infos 2>$OUT_PREFIX.fill.log || exit 1
-
-if [[ ! -f $OUT_PREFIX.contig ]] ; then
-    ln -s $CONTIG $OUT_PREFIX.contig || exit 1
-else
-    echo "Warning : $OUT_PREFIX.contig already exist !!! use it anyway ."
-fi
 
 $BIN_DIR/ScaffInfo2Seq --prefix  $OUT_PREFIX --min_n 1 --min_c 0 \
     >$OUT_PREFIX.i2s.log 2>&1  || exit 1
-
